ec7497d2b3a5494429dae9f2970d4233fdb4a3b5
semxx <semx@mail.ru>
2015-10-23 14:53:00 +0300

    Create LDR_alarm_V5.ino

---
+++ LDR_alarm_V5.ino
@@ -0,0 +1,29 @@
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "8d82695b6d7cxxxxxxxxx";
+int rcvpin=A0;
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth, "TP-LINK", "yyy");
+   pinMode(rcvpin,INPUT);
+}
+  
+void loop() {  
+   int ldrval=analogRead(rcvpin);
+if(ldrval<100)
+
+{
+Blynk.virtualWrite(5, HIGH);
+}
+else {
+     
+      Blynk.virtualWrite(5, LOW);
+      }
+    
+    Blynk.run();
+     }
+   
+
