5890f63aae11aef64d5bedbca6abb55f7761f30a
semxx <semx@mail.ru>
2017-01-12 17:15:07 +0300

    Update MyPID.ino

--- SmartHouse/MyPID.ino
+++ SmartHouse/MyPID.ino
@@ -1,39 +1,34 @@
 /********************************************************
- * PID RelayOutput Example
- * Same as basic example, except that this time, the output
- * is going to a digital pin which (we presume) is controlling
- * a relay.  The pid is designed to output an analog value,
- * but the relay can only be On/Off.
- *
- *   To connect them together we use "time proportioning
- * control"  Tt's essentially a really slow version of PWM.
- * First we decide on a window size (5000mS say.) We then 
- * set the pid to adjust its output between 0 and that window
- * size.  Lastly, we add some logic that translates the PID
- * output into "Relay On Time" with the remainder of the 
- * window being "Relay Off Time"
+ * PID Adaptive Tuning Example
+ * One of the benefits of the PID library is that you can
+ * change the tuning parameters at any time.  this can be
+ * helpful if we want the controller to be agressive at some
+ * times, and conservative at others.   in the example below
+ * we set the controller to use Conservative Tuning Parameters
+ * when we're near setpoint and more agressive Tuning
+ * Parameters when we're farther away.
  ********************************************************/
 
 #include <PID_v1.h>
-#define RelayPin 6
+
+#define PIN_INPUT 0
+#define PIN_OUTPUT 3
 
 //Define Variables we'll be connecting to
 double Setpoint, Input, Output;
 
-//Specify the links and initial tuning parameters
-PID myPID(&Input, &Output, &Setpoint,2,5,1, DIRECT);
+//Define the aggressive and conservative Tuning Parameters
+double aggKp=4, aggKi=0.2, aggKd=1;
+double consKp=1, consKi=0.05, consKd=0.25;
 
-int WindowSize = 5000;
-unsigned long windowStartTime;
+//Specify the links and initial tuning parameters
+PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, DIRECT);
+
 void setup()
 {
-  windowStartTime = millis();
-
   //initialize the variables we're linked to
+  Input = analogRead(PIN_INPUT);
   Setpoint = 100;
-
-  //tell the PID to range between 0 and the full window size
-  myPID.SetOutputLimits(0, WindowSize);
 
   //turn the PID on
   myPID.SetMode(AUTOMATIC);
@@ -41,18 +36,19 @@ void setup()
 
 void loop()
 {
-  Input = analogRead(0);
-  myPID.Compute();
+  Input = analogRead(PIN_INPUT);
 
-  /************************************************
-   * turn the output pin on/off based on pid output
-   ************************************************/
-  unsigned long now = millis();
-  if(now - windowStartTime>WindowSize)
-  { //time to shift the Relay Window
-    windowStartTime += WindowSize;
+  double gap = abs(Setpoint-Input); //distance away from setpoint
+  if (gap < 10)
+  {  //we're close to setpoint, use conservative tuning parameters
+    myPID.SetTunings(consKp, consKi, consKd);
   }
-  if(Output > now - windowStartTime) digitalWrite(RelayPin,HIGH);
-  else digitalWrite(RelayPin,LOW);
+  else
+  {
+     //we're far from setpoint, use aggressive tuning parameters
+     myPID.SetTunings(aggKp, aggKi, aggKd);
+  }
 
+  myPID.Compute();
+  analogWrite(PIN_OUTPUT, Output);
 }
