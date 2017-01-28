76b1f363a302676671797855025bcdd966029538
semxx <semx@mail.ru>
2017-01-29 01:50:27 +0300

    Update Time.ino

--- SmartHouse/Time.ino
+++ SmartHouse/Time.ino
@@ -15,6 +15,7 @@ void AdjustTime(){
   }
 void PrintRTC(char x, char y)
 {
+  display.clearDisplay();
   display.setTextSize(2);
   display.setCursor(x, y);
   
@@ -65,57 +66,65 @@ void PrintRTC(char x, char y)
     }
     display.print("]");    //    lcd.print(F("]"));
   }
- 
+ display.display();
+ timer.run();
 }
 void SetTime(char x, char y)
 {
   // ========= set hours
   SetH = true;
-  do {
+  do
+  {
+     
     PrintRTC(x,y);
     rotating = true;  // reset the debouncer
-    if (encoderR) {
+if (encoderValue > oldEncoderValue){
       Hours += 1;
       if(Hours > 23) {
         Hours = 0;
       };
-      encoderR = false;
+      oldEncoderValue = encoderValue;
     }
-    if (encoderL) {
+if (encoderValue < oldEncoderValue) {
       Hours -= 1;
       if(Hours < 0) {
         Hours = 23;
       };
-      encoderL = false;
+      oldEncoderValue = encoderValue;
     }
-    timer.run();
+Serial.println(MenuTimeoutTimer);
+Serial.println(digitalRead(btn_Right));    
+//    timer.run();
   }
-  while ((digitalRead(btn_Right)==1)|(MenuTimeoutTimer==0));
+while ((digitalRead(btn_Right)==1)|(MenuTimeoutTimer==0));  
+
   //if (BeepEnabled) {
   //  tone(BeepPin,4000,50); //звук "YES"
   
  SetH = false;
-  delay(200);
+  //delay(200);
   // ========= set minutes
   SetM = true;
   do {
+
     PrintRTC(0,0);
     rotating = true;  // reset the debouncer
-    if (encoderR) {
+if (encoderValue > oldEncoderValue){
       Minutes += 1;
       if(Minutes > 59) {
         Minutes = 0;
       };
-      encoderR = false;
+      oldEncoderValue = encoderValue;
     }
-    if (encoderL) {
+if (encoderValue < oldEncoderValue){
       Minutes -= 1;
       if(Minutes < 0) {
         Minutes = 59;
       };
-      encoderL = false;
+      oldEncoderValue = encoderValue;
     }
-  timer.run();
+ 
+//  timer.run();
   }
   while ((digitalRead(btn_Right)==1)|(MenuTimeoutTimer==0));
  // if (BeepEnabled) {
@@ -123,20 +132,19 @@ void SetTime(char x, char y)
   
   if (PrintYesNo) {
     SetM = false;
-    delay(200);
+    //delay(200);
     // ========= set yes-no
     SetYesNo = false;
     do {
       PrintRTC(0,0);
       rotating = true;  // reset the debouncer
-      if ((encoderR)||(encoderL)) {
+      if (encoderValue  != oldEncoderValue){
         SetYesNo = !SetYesNo;
-        encoderR = false;
-        encoderL = false;
+      oldEncoderValue = encoderValue;
       }
-    timer.run();
+//    timer.run();
     }
     while ((digitalRead(btn_Right)==1)|(MenuTimeoutTimer==0));
-    delay(200);
-  }
+    //delay(200);
+  } 
 }
