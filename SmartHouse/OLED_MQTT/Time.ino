01a831ec0fca34542a1fe8c8937f78e13884874a
semxx <semx@mail.ru>
2018-11-15 23:32:33 +0300

    Create Time.ino

---
+++ SmartHouse/OLED_MQTT/Time.ino
@@ -0,0 +1,155 @@
+
+void timerHalfSec()
+{
+  blink500ms = !blink500ms; // инверсия мерцающего бита
+  if(blink500ms) {
+    plus1sec = true; // ежесекундно взводится
+    if (MenuTimeoutTimer != 0) {
+      MenuTimeoutTimer --; // ежесекундный декремент этого таймера
+    }
+  }
+}
+
+void AdjustTime(){
+
+  }
+void PrintRTC(char x, char y)
+{
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setCursor(x, y);
+  
+  if (SetH&&blink500ms) {
+    display.print("  ");    //  lcd.print(F("  "));
+  }
+  else {
+    if (Hours < 10) {
+      display.print("0");   //  lcd.print(F("0"));
+    }   
+    display.print(Hours);   //  lcd.print(Hours);
+  }
+ 
+  // мигающее двоеточие, если не в режиме установки времени
+  if (!(SetH||SetM||PrintYesNo||blink500ms))
+  {
+     display.print(" ");  //  lcd.print(F(" "));
+  }
+  else {
+     display.print(":");   //  lcd.print(F(":"));
+  }
+ 
+  if (SetM&&blink500ms) {
+    display.print("  ");    //      lcd.print(F("  "));
+  }
+  else {
+    if (Minutes < 10) {
+     display.print("0");   //  lcd.print(F("0"));
+    }   
+     display.print(Minutes);   //      lcd.print(Minutes);
+  }
+     display.print(" ");  //  lcd.print(F(" "));
+ 
+  if (PrintYesNo) {
+    display.print("[");    //    lcd.print(F("["));
+    if (!(SetH||SetM||blink500ms))
+    {
+    display.print("  ");    //      lcd.print(F("   "));
+    }
+    else {
+      if (SetYesNo)
+      {
+    display.print("YES");    //        lcd.print(F("YES"));
+      }
+      else {
+    display.print("NO");    //        lcd.print(F("NO "));
+      }
+    }
+    display.print("]");    //    lcd.print(F("]"));
+  }
+ display.display();
+ timer.run();
+}
+void SetTime(char x, char y)
+{
+  // ========= set hours
+  SetH = true;
+  do
+  {
+    ESP.wdtDisable();
+    PrintRTC(x,y);
+    rotating = true;  // reset the debouncer
+    if (encoderValue > oldEncoderValue){
+      Hours += 1;
+      if(Hours > 23) {
+        Hours = 0;
+      };
+      oldEncoderValue = encoderValue;
+    }
+    if (encoderValue < oldEncoderValue) {
+      Hours -= 1;
+      if(Hours < 0) {
+        Hours = 23;
+      };
+      oldEncoderValue = encoderValue;
+    }
+    if(Connected2Blynk){
+      Blynk.run();  // only process Blyk.run() function if we are connected to Blynk server
+    }
+    timer.run();
+  }
+while ((MenuTimeoutTimer==0) || (digitalRead(Encoder_SW)==0));  
+
+  //if (BeepEnabled) {
+  //  tone(BeepPin,4000,50); //звук "YES"
+  
+ SetH = false;
+  delay(200);
+  // ========= set minutes
+  SetM = true;
+  do {
+
+    PrintRTC(0,0);
+    rotating = true;  // reset the debouncer
+    if (encoderValue > oldEncoderValue){
+      Minutes += 1;
+      if(Minutes > 59) {
+        Minutes = 0;
+      };
+      oldEncoderValue = encoderValue;
+    }
+    if (encoderValue < oldEncoderValue){
+      Minutes -= 1;
+      if(Minutes < 0) {
+        Minutes = 59;
+      };
+      oldEncoderValue = encoderValue;
+    }
+ 
+  timer.run();
+  }
+  while ((digitalRead(Encoder_SW)==0)||(MenuTimeoutTimer==0));
+ // if (BeepEnabled) {
+ //   tone(BeepPin,4000,50); //звук "YES"
+  
+  if (PrintYesNo) {
+    SetM = false;
+    delay(200);
+    // ========= set yes-no
+    SetYesNo = false;
+    do {
+      ESP.wdtDisable();
+      PrintRTC(0,0);
+      rotating = true;  // reset the debouncer
+      if (encoderValue  != oldEncoderValue){
+        SetYesNo = !SetYesNo;
+      oldEncoderValue = encoderValue;
+      }
+    timer.run();
+    }
+    while ((digitalRead(Encoder_SW)==0)|(MenuTimeoutTimer==0));
+    //delay(200);
+  }
+MyPrint(F("Saved.."), 1 * 6 - 6, 3 * 8 - 8, 3, 1);   
+num_Screen = 1;
+ESP.wdtEnable(15000);
+}
